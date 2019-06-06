00647002952c0ea8d1b89b8c06c67a0e0daa65c9
Janik Walter Andrejkovic <jandrejkovic@gmx.net>
2019-06-06 12:37:04 +0200

    removing userdefined input files (for JEC) + adapt from 2018 cfg to 2016

--- run/crab_submit_template.py
+++ run/crab_submit_template.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 tag = "${tag}"
-prodLabel = 'ReRunNano2018_190415'
+prodLabel = 'ReRunNano2016_190605'
 file_dataset = "${sample}"
 # General section - user specifies generic parameters
 config.General.requestName = tag
@@ -13,9 +13,7 @@ config.General.workArea = 'crab_{0}'.format( prodLabel )
 config.JobType.pluginName = 'Analysis'
 config.JobType.maxMemoryMB = 2000
 # config.JobType.maxJobRuntimeMin = 1440
-config.JobType.psetName = "HIG-RunIIAutumn18MiniAOD_${datatier}_cfg.py"
-config.JobType.inputFiles = ["/afs/hephy.at/work/j/jandrejkovic/2018nanoProd/CMSSW_10_2_10/src/WawTools/NanoAODTools/run/Autumn18_V3_MC.db",
-							 "/afs/hephy.at/work/j/jandrejkovic/2018nanoProd/CMSSW_10_2_10/src/WawTools/NanoAODTools/run/Autumn18_RunABCD_V8_DATA.db"]
+config.JobType.psetName = "HIG-RunIISummer16MiniAODv3_${datatier}_cfg.py"
 # Data section - contains all paramters related to the data to be analyzed
 config.Data.inputDataset = file_dataset
 config.Data.splitting = 'FileBased' 
